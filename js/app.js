df967703e5eff61cbd0ef21d70d3abab148f898d
Derek J Leong <derekjleong@gmail.com>
2019-01-15 08:51:17 -0800

    General Progress -tweaked gears spinning

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
 window.onscroll = function rotateGear () {
-    document.querySelector(".gear1").style.transform = "rotate(" + (window.scrollY * 5) + "deg)";
-    document.querySelector(".gear2").style.transform = "rotate(-" + (window.scrollY * 5) + "deg)";
+    document.querySelector(".gear1").style.transform = "rotate(" + (window.scrollY * 4) + "deg)";
+    document.querySelector(".gear2").style.transform = "rotate(-" + (window.scrollY * 4) + "deg)";
   }
