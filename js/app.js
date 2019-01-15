79c6b80c1337239074f3d163bba97aeb91448ec1
Derek J Leong <derekjleong@gmail.com>
2019-01-14 23:43:54 -0800

    -animation edit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
 window.onscroll = function rotateGear () {
-    document.querySelector(".gear1").style.transform = "rotate(" + (window.scrollY * 3.5) + "deg)";
-    document.querySelector(".gear2").style.transform = "rotate(-" + (window.scrollY * 3.5) + "deg)";
+    document.querySelector(".gear1").style.transform = "rotate(" + (window.scrollY * 5) + "deg)";
+    document.querySelector(".gear2").style.transform = "rotate(-" + (window.scrollY * 5) + "deg)";
   }
