9dd492208c82aac199171614781ff30829e1bf17
Derek J Leong <derekjleong@gmail.com>
2019-01-26 16:07:32 -0800

    New Imgs and Reformatting Added icon carousel to design section

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
+// For Gear Scroll Animation
 window.onscroll = function rotateGear () {
     document.querySelector(".gear1").style.transform = "rotate(" + (window.scrollY * 4) + "deg)";
     document.querySelector(".gear2").style.transform = "rotate(-" + (window.scrollY * 4) + "deg)";
-  }
+}
+
+// For Design Section Icon Carousel
+var imgArray = [
+  'img/design64x64.png',
+  'img/responsive64x64.png',
+  'img/happiness64x64.png'],
+  curIndex = 0;
+  imgDuration = 5000;
+
+function slideShow() {
+  document.getElementById('des__icon').className += "fadeOut";
+  setTimeout(function() {
+      document.getElementById('des__icon').src = imgArray[curIndex];
+      document.getElementById('des__icon').className = "";
+  },1000);
+  curIndex++;
+  if (curIndex == imgArray.length) { curIndex = 0; }
+  setTimeout(slideShow, imgDuration);
+}
+slideShow();
