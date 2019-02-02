7be11ec9fa24bd29bb5a18beb22d64c3da41c55c
Derek J Leong <derekjleong@gmail.com>
2019-02-01 18:06:47 -0800

    nav in functioning state added styling added javascript to handle clicks

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
+//For nav
+document.getElementById("nav__open").addEventListener("click", function () {
+  console.log('clicked');
+  document.getElementById("nav").classList.toggle("nav__show");
+  console.log('toggled');
+});
+document.getElementById("nav__close").addEventListener("click", function () {
+  console.log('clicked');
+  document.getElementById("nav").classList.toggle("nav__show");
+});
+
+let navList = document.querySelectorAll("nav a");
+for (var i = 0; i < navList.length; i++) {
+  navList[i].addEventListener("click", function () {
+    console.log('clicked');
+    document.getElementById("nav").classList.toggle("nav__show");
+  });
+}  
+
 // For Gear Scroll Animation
 window.onscroll = function rotateGear () {
     document.querySelector(".gear1").style.transform = "rotate(" + (window.scrollY * 4) + "deg)";
@@ -54,3 +73,4 @@ function mouseOutHello(id) {
 }
 document.getElementById("hello").addEventListener("mouseover", mouseOverHello);
 document.getElementById("hello").addEventListener("mouseout", mouseOutHello);
+
